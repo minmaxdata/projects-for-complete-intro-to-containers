612cf3fb40ae0feb54ae9e7dd086fddb4fd69bb3
KE McAdams <kemcadams@gmail.com>
2020-11-16 15:05:25 -0800

    edits during video

--- namespaces/setup.sh
+++ namespaces/setup.sh
@@ -7,7 +7,8 @@ debootstrap --variant=minbase bionic /better-root
 
 echo "mount -t proc none /proc # process namespace
 mount -t sysfs none /sys # filesystem
-mount -t tmpfs none /tmp # filesystem" >> /better-root/mounts.sh
+mount -t tmpfs none /tmp # filesystem" >> /betps aux
+ter-root/mounts.sh
 
 # head into the new namespace'd, chroot'd environment
 unshare --mount --uts --ipc --net --pid --fork --user --map-root-user chroot /better-root bash # this also chroot's for us
